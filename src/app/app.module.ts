5c2bdc082a1ca8258f7eda3cddc600d87eb1ef1f
Shilpa Chelagamsetti <1681523@TCSNSN.COM>
2020-01-09 11:43:19 +0530

    removedAgm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { CountriesComponent } from './countries/countries.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 
 import {DragDropModule} from '@angular/cdk/drag-drop';
-import { AgmCoreModule } from '@agm/core';
+
 import { CountryInDetailComponent } from './country-in-detail/country-in-detail.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -27,13 +27,6 @@ import { LoginComponent } from './login/login.component';
     AppRoutingModule,
     HttpClientModule,
     DragDropModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyDiASazcyivjbEs8FhyyU09tO1qU7xIpQs'
-      /* apiKey is required, unless you are a
-      premium customer, in which case you can
-      use clientId
-      */
-    }),
     NgCircleProgressModule.forRoot({
       // set defaults here
       radius: 100,
